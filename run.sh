e5794dd026b7054acc5990b7f84700ebf750ba51
kim-al <kim-al>
2021-07-27 12:03:50 +0300

    first commit

---
+++ run.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+OUT_DIR="wordcount_result_"$(date +"%s%6N")
+NUM_REDUCERS=8
+
+hdfs dfs -rm -r -skipTrash ${OUT_DIR} > /dev/null
+
+yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
+    -D mapred.job.name="Streaming wordCount" \
+    -D mapreduce.job.reduces=${NUM_REDUCERS} \
+    -files mapper.py,reducer.py \
+    -mapper "python mapper.py" \
+    -combiner "python reducer.py" \
+    -reducer "python reducer.py" \
+    -input /data/wiki/en_articles_part \
+    -output ${OUT_DIR} > /dev/null
+
+hdfs dfs -cat ${OUT_DIR}/part-00000 | head
